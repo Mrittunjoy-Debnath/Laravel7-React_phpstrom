36b5faedd9dd4f271b59fd0aa8653887f0447ac7
Mrittunjoy Debnath <shuvo.cse2014@gmail.com>
2022-01-13 13:12:37 +0600

    adding css,responsive css and images

--- resources/js/App.js
+++ resources/js/App.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import Menu from "./components/menu";
+import burger from "../images/burger.jpg";
+import "../css/style.css";
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Menu/>
+                <img src={burger}/>
+                <button className="normal-btn btn">Login Btn</button>
             </div>
         );
     }
